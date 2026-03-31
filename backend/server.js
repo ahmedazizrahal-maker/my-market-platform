16a471351f90c6cc49aa519830c62586bd86bd23
ahmedazizrahal-maker <ahmed.aziz.rahal@gmail.com>
2026-03-31 14:21:51 -0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ const productRoutes = require("./routes/products");
 const adRoutes = require("./routes/ads");
 const checkoutRoutes = require("./routes/checkout");
 const { requireAuth } = require("./middleware/auth");
+const uploadRoutes = require("./routes/upload");
 
 
 const app = express();
@@ -25,6 +26,7 @@ app.use("/api/vendors", vendorRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/ads", adRoutes);
 app.use("/api/checkout", checkoutRoutes);
+app.use("/api/upload", uploadRoutes);
 
 app.get("/api/debug-token", requireAuth , (req, res) => {
   res.json({ userFromToken: req.user });
