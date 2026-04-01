f85cc107924fe91b56ff9e6f17e9e798f5fbce67
ahmedazizrahal-maker <ahmed.aziz.rahal@gmail.com>
2026-04-01 12:47:38 -0700

    Update Product.js

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -4,8 +4,8 @@ const ProductSchema = new mongoose.Schema({
   vendorId: { type: mongoose.Schema.Types.ObjectId, ref: "Vendor", required: true },
   title: { type: String, required: true },
   description: String,
-  //images: [String],
-  imageUrl: String,
+  images: [String],
+  //imageUrl: String,
   basePrice: { type: Number, required: true },
   currentPrice: { type: Number, required: true },
   sku: { type: String, index: true }, // used for comparison
